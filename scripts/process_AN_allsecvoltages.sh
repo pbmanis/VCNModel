252b1d0d1c438a99a529936957ade85aa3cd7b8b
Paul B. Manis <pmanis@med.unc.edu>
2020-02-04 17:37:06 -0500

    add inits

--- scripts/process_AN_allsecvoltages.sh
+++ scripts/process_AN_allsecvoltages.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
-python vcnmodel/model_run.py VCN_c09 --type Bushy --modeltype II --protocol initAN --hocfile VCN_c09_FullCell_edited.hoc --model XM13_nacncoop \
-         -r 1 --sgcmodel cochlea -S MS -f 16000. --soma_autoinflate --dendrite_autoinflate\
-         --tagstring noDendNa\
-         --saveall --noparallel
+# python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol initIV --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+#          -r 1 \
+#          --tagstring noDendNa\
+#          --saveall --noparallel\
+#          #--soma_autoinflate --dendrite_autoinflate
+#
+# python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol runIV --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+#          -r 1 \
+#          --tagstring noDendNa\
+#          --saveall --noparallel\
+#         #--soma_autoinflate --dendrite_autoinflate
+#
+# echo IV saveallrun complete
 
+python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol initIV --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+         -r 1 \
+         --tagstring noDendNa_Full
+         --saveall --noparallel\
+         #--soma_autoinflate --dendrite_autoinflate
 
-python vcnmodel/model_run.py VCN_c09 --type Bushy --modeltype II --protocol runANPSTH --hocfile VCN_c09_FullCell_edited.hoc --model XM13_nacncoop \
-         -r 1 --sgcmodel cochlea -S MS -f 16000. --soma_autoinflate --dendrite_autoinflate\
-         --tagstring noDendNa\
-         --saveall --noparallel
+python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol runIV --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+         -r 1 \
+         --tagstring noDendNa_Full\
+         --saveall --noparallel\
+        #--soma_autoinflate --dendrite_autoinflate
 
-wait
-echo AN saveallrun complete
+echo IV saveallrun complete
+
+# python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol initAN --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+#          -r 1 --sgcmodel cochlea -S MS -f 16000. \
+#          --saveall --noparallel \
+#          --tagstring noDendNa \
+#              # --soma_autoinflate --dendrite_autoinflate\
+#     #
+#
+#
+# python vcnmodel/model_run.py VCN_c06 --type Bushy --modeltype II --protocol runANPSTH --hocfile VCN_c06_Full.hocx --model XM13_nacncoop \
+#          -r 1 --sgcmodel cochlea -S MS -f 16000. \
+#          --tagstring noDendNa \
+#          --saveall --noparallel \
+#         #     --soma_autoinflate --dendrite_autoinflate\
+#
+# wait
+# echo AN saveallrun complete
 
 
