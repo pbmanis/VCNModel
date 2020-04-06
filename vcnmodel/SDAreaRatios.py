b26f51e89b35ab184c0197c3f65b4c62362775aa
pbmanis <pmanis@med.unc.edu>
2020-04-06 09:48:25 -0400

    updating area calculations and formatting

--- vcnmodel/SDAreaRatios.py
+++ vcnmodel/SDAreaRatios.py
@@ -7,40 +7,15 @@ from collections import OrderedDict
 import pyqtgraph as pg
 from cnmodel import cells
 from cnmodel.decorator import Decorator
+import adjust_areas
 
-# from neuronvis import HocViewer
-gradeA = [2, 5, 6, 9, 10, 11, 13,  24, 29, 30] 
-allcells = [
-    "02",
-    "05",
-    "06",
-#    "08",
-    "09",
-    "10",
-    "11",
-    "13",
-    # "14",
-    # "16",
-    "17",
-    # "18",
-    # "19",
-    # "20",
-    # "21",
-    # "22",
-    "24",
-    "29",
-    "30",
-]
-#allcells = ['09']
+AdjA = adjust_areas.AdjustAreas()
 
-def sum_area(areamap):
-    # areamap is post_cell.areaMap[sectiontype]
-    # is still a dict{ 'section[x]': floatingareavalue}
+gradeA = [2, 5, 6, 9, 10, 11, 13, 24, 29, 30]
 
-    area = 0.0
-    for x in areamap:
-        area += float(areamap[x])
-    return area
+all_cell_nos = range(1, 32)
+allcells = [f"{c:02d}" for c in all_cell_nos]
+print(allcells)
 
 
 def sum_area(areamap):
@@ -51,18 +26,25 @@ def sum_area(areamap):
     for x in areamap:
         area += float(areamap[x])
     return area
+
 
 def one_sectype_area(amap):
-    area = 0.
+    area = 0.0
     area = np.sum([amap[s] for s in amap])
-    print(area)
+    # print(area)
     return area
 
+
 def area(fn):
-    filename = Path("/Users/pbmanis/desktop/Python/VCN-SBEM-Data/VCN_Cells", fn, "Morphology", fn +  "_full.hoc")
+    filename = Path(
+        "/Users/pbmanis/desktop/Python/VCN-SBEM-Data/VCN_Cells",
+        fn,
+        "Morphology",
+        fn + "_full.hoc",
+    )
     post_cell = cells.Bushy.create(
         morphology=str(filename),
-        #decorator=Decorator,
+        # decorator=Decorator,
         species="mouse",
         modelType="II",
         modelName="XM13",
@@ -71,7 +53,7 @@ def area(fn):
     post_cell.distances()
     post_cell.computeAreas()
     secareas = {}
-    print('soma: ', post_cell.areaMap['soma'])
+    # print('soma: ', post_cell.areaMap['soma'])
     for am in list(post_cell.areaMap.keys()):
         print('am: ', am)
         if am not in list(secareas.keys()):
@@ -81,16 +63,16 @@ def area(fn):
     # for s in secareas.keys():
     #     print(f"    first pass {s:>24s} = {secareas[s]:.3f}")
 
-    # now collapse dendrites... 
-    secareas['somabysegment'] = 0.
-    secareas['dendrite'] = 0.
+    # now collapse dendrites...
+    secareas["somabysegment"] = 0.0
+    secareas["dendrite"] = 0.0
     # secareas['soma'] = 0.
     for am in list(post_cell.areaMap.keys()):
         # if am == "soma":
         #     secareas["soma"] += one_sectype_area(post_cell.areaMap[am])
-        print('am: ', am)
-        if am.startswith('Dendr') or am.endswith('Dendrite') or am.startswith('dend'):
-            print('adding dendr area: ', am, one_sectype_area(post_cell.areaMap[am]))
+        # print('am: ', am)
+        if am.startswith("Dendr") or am.endswith("Dendrite") or am.startswith("dend"):
+            print("adding dendr area: ", am, one_sectype_area(post_cell.areaMap[am]))
             secareas["dendrite"] += one_sectype_area(post_cell.areaMap[am])
     print(f"{filename.name:s}   secareas: {str(secareas):s}")
     return secareas
@@ -99,12 +81,15 @@ def area(fn):
 if __name__ == "__main__":
 
     ar = OrderedDict()
-    for fn in allcells:
+    for i, fn in enumerate(allcells):
         filename = (
             "VCN_c" + fn
         )  # os.path.join('VCN_Cells', 'VCN_c'+fn, 'Morphology', 'VCN_c'+fn+'.hoc')
-        print('making fn: ', fn)
-        ar[fn] = area(filename)
+        if i + 1 in gradeA:
+            print("getting area for fn: ", fn)
+            ar[fn] = area(filename)
+        else:
+            ar[fn] = {"soma": np.nan, "dendrite": np.nan}
     print("")
     hdrstr = [
         "Cell",
@@ -134,16 +119,20 @@ if __name__ == "__main__":
         # print(ar[fn].keys())
         ar[fn]["ratio"] = ar[fn]["dendrite"] / ar[fn]["soma"]
         txt = "{:^12s}  ".format("VCN_c{0:2s}".format(fn))
+        if i + 1 == 29:
+            print(f"{fn:^12s}.D02")
         for ik, k in enumerate(hdrkeys):
             if k not in list(ar[fn].keys()):
                 continue
-                #txt += "{:>12.{:d}f}  ".format(0.0, dec[ik])
+                # txt += "{:>12.{:d}f}  ".format(0.0, dec[ik])
             # elif k == "somabysegment":
             #     txt += "{:>12.{:d}f}  ".format(ar[fn][k], dec[ik])
             elif k == "":
                 continue
             else:
-                txt += f"{ar[fn][k]:>12.{dec[ik]:d}f}  " # .format(ar[fn][k], dec[ik])
+                txt += f"{ar[fn][k]:>12.{dec[ik]:d}f}  "  # .format(ar[fn][k], dec[ik])
         print(txt)
+        if i + 1 == 24:
+            print(f"{fn:^12s}.D01")
         # print ('VCN_c{0:2s}:  Somatic area: {1:>8.2f}   Dendritic area: {2:>8.2f}  Ratio: {3:>5.3f}'
         #     .format(fn, ar[fn]['soma'], ar[fn]['dendrite'], ar[fn]['dendrite']/ar[fn]['soma']))
