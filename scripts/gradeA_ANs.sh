81417c62baf302e7cb1f3f7392a014c6d8c4dde7
pbmanis <pmanis@med.unc.edu>
2020-05-03 10:07:35 -0400

    simple synapse configuration file and running script

---
+++ scripts/gradeA_ANs.sh
@@ -0,0 +1,26 @@
+# Example:
+# scripts/process_gbcIV.sh run all 
+#
+
+#######################################################
+# Full models are from data/reconstuctions Matthew Kersting sent on
+# March 6, 2020. 
+# Note we do not have a full reconstruction for cell 18
+# in that dataset.
+#######################################################
+CELLNAMES="09" # "02 05 06 09 10 11 13 17 24 29 30"
+CONFIG="noscale.toml" #"autoscale.toml"
+#CONFIG="autoscale_simple.toml"
+echo "running the individual initialization and running AN PSTH protocols"
+for f in $CELLNAMES
+do
+    echo $f
+    python vcnmodel/model_run2.py VCN_c$f -i VCN_c09 -F -P initAN --configfile $CONFIG
+    python vcnmodel/model_run2.py VCN_c$f -i VCN_c09 -F -P runANPSTH -r 1 --configfile $CONFIG
+done
+
+wait
+
+echo AN runs complete
+# with "A", we do all cells in grade A
+# python vcnmodel/plotters/plot_gbc_ivresults_2.py "A" -s
