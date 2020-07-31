109ea1f74f3de14509a2ee35a4609dad3b5c4002
pbmanis <pmanis@med.unc.edu>
2020-07-31 07:25:35 -0400

    new data control tables for model and associated config files

--- vcnmodel/model_data/data_XM13A_nacncoop_actdend.py
+++ vcnmodel/model_data/data_XM13A_nacncoop_actdend.py
@@ -20,20 +20,21 @@ ChannelData = u"""
     -----------------------------------------------------------------------------------------------------------------------------------
                       II                   II-I                I-c                I-II                I-t       
                                                                                                     
-    nacncoop_gbar     1000.        [1]     1000.       [1]     3000.       [1]    1000.        [2]    3000.  [1] 
-    kht_gbar          29.0         [3]     58.0        [1]     500.0       [1]    150.0        [2]    500.0  [1] 
-    klt_gbar          80.0         [1]     14.0        [1]     0.0         [1]    20.0         [2]    0.0    [1] 
-    ka_gbar           0.0          [1]     0.0         [1]     0.0         [1]    0.0          [2]    125.0    [1] 
-    ihvcn_gbar        15.0         [1]     30.0        [1]     18.0        [1]    2.0          [2]    18.0   [1] 
-    leak_gbar         1.0          [1]     2.0         [1]     8.0         [1]    2.0          [2]    8.0    [1] 
+    nacncoop_gbar     20.124       [1]     1000.       [1]     3000.       [1]    1000.        [2]    3000.  [1] 
+    kht_gbar          1.1672       [3]     58.0        [1]     500.0       [1]    150.0        [2]    500.0  [1] 
+    klt_gbar          1.6100       [1]     14.0        [1]     0.0         [1]    20.0         [2]    0.0    [1] 
+    ka_gbar           0.0000       [1]     0.0         [1]     0.0         [1]    0.0          [2]    125.0    [1] 
+    ihvcn_gbar        0.6037       [1]     30.0        [1]     18.0        [1]    2.0          [2]    18.0   [1] 
+    leak_gbar         0.1385       [1]     2.0         [1]     8.0         [1]    2.0          [2]    8.0    [1] 
     leak_erev         -65          [1]     -65         [1]     -65         [1]    -65          [2]    -65    [1] 
     na_type           nacncoop     [1]     nacncoop    [1]     nacncoop    [1]    nacncoop     [1]    nacncoop   [1] 
     ih_type           ihvcn        [1]     ihvcn       [1]     ihvcn       [1]    ihvcn        [2]    ihvcn  [1] 
-    soma_Cap          26.0         [1]     26.0        [1]     25.0        [1]    26.0         [2]    25.0   [1] 
+    soma_Cap          13.0         [1]     26.0        [1]     25.0        [1]    26.0         [2]    25.0   [1] 
     e_k               -84          [1]     -84         [1]     -84         [1]    -84          [2]    -84    [1] 
     e_na              50.          [1]     50.         [1]     50.         [1]    50.          [2]    50.    [1] 
     ih_eh             -43          [1]     -43         [1]     -43         [1]    -43          [2]    -43    [1] 
     nacncoop_vshift   0.     [1]           0.     [1]          0.     [1]         0.     [1]          0.     [1]
+    units             mmho/cm2             nS                  nS                 nS                  nS
 
     -----------------------------------------------------------------------------------------------------------------------------------
 
@@ -56,34 +57,6 @@ ChannelData = u"""
     """
 
 
-# ChannelCompartments = u"""
-#
-#     This table describes the ion channel densities relative to somatic densities,
-#     e.g., relative to REFERENCE densities in the table XM13_channels.
-#     and voltage shifts, for different compartments of the specified neuron,
-#     Conductances will be calculated from the Model derived from Xie and Manis 2013 for mouse
-#     (data table: XM13_channels).
-#
-#     NOTE: unmyelinatedaxon and initialsegment are equivalent in George's models, but only "unmyelinatedaxon" is actually used.
-#     ------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#                        axon       unmyelinatedaxon     myelinatedaxon     initialsegment    hillock     soma        dendrite         primarydendrite    secondarydendrite
-#
-#     nacncoop_gbar      3.0 [1]    15. [1]              0.0 [1]            0.0 [1]           9.0 [1]     2.5 [1]     0.5 [1]          0.25 [1]           0.25 [1]
-#     kht_gbar           1.0 [1]    1.0 [1]              0.01 [1]           2.0 [1]           1.0 [1]     1.0 [1]     1.0 [1]          0.5 [1]            0.25 [1]
-#     klt_gbar           1.0 [1]    1.0 [1]              0.01 [1]           1.0 [1]           1.0 [1]     1.0 [1]     0.5 [1]          0.5 [1]            0.25 [1]
-#     ihvcn_gbar         0.0 [1]    0.0 [1]              0.0 [1]            0.5 [1]           0.0 [1]     1.0 [1]     0.5 [1]          0.5 [1]            0.5 [1]
-#     leak_gbar          1.0 [1]    0.25 [1]             0.25e-3 [1]        1.0 [1]           1.0 [1]     1.0 [1]     0.5 [1]          0.5 [1]            0.5 [1]
-#     leak_erev          -65. [1]   -65. [1]             -65. [1]           -65. [1]          -65. [1]    -65. [1]    -65. [1]         -65. [1]           -65. [1]
-#     na_type            nacncoop   nacncoop             nacncoop           nacncoop          nacncoop    nacncoop    nacncoop         nacncoop           nacncoop
-#     nacncoop_vshift    0.  [2]    0. [2]               0. [2]             0. [2]            0. [2]      0. [2]      0.  [2]          0.  [2]             0. [2]
-#     ih_type            ihvcn      ihvcn                ihvcn              ihvcn             ihvcn       ihvcn       ihvcn            ihvcn              ihvcn
-#     -------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#
-#     [1] Scaling is relative to soma scaling. Numbers are estimates based on general distribution from literature on cortical neurons.
-#     [2] Set to 0 (was 4.3 in original model). Matches original Barela et al (2006) scaling.
-#
-#     """
-
 ChannelCompartments = u"""
 
     This table describes the ion channel densities relative to somatic densities,
@@ -96,11 +69,11 @@ ChannelCompartments = u"""
     --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
                        axon       Unmyelinated_Axon    Myelinated_Axon    Axon_Initial_Segment    Axon_Hillock     soma        Proximal_Dendrite     Distal_Dendrite    Dendritic_Hub     Dendritic_Swelling
                                                                                                                                                       
-    nacncoop_gbar      3.0 [1]    15. [1]              0.0 [1]            15. [1]                 9.0 [1]          2.5 [1]     2.5 [1]               2.5 [1]            2.5 [1]           2.5 [1] 
-    kht_gbar           1.0 [1]    1.0 [1]              0.01 [1]           2.0 [1]                 1.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
+    nacncoop_gbar      1.0 [1]    100.0 [1]            0.0 [1]            100.0 [1]               5.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
+    kht_gbar           1.0 [1]    2.0 [1]              0.01 [1]           2.0 [1]                 1.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
     klt_gbar           1.0 [1]    1.0 [1]              0.01 [1]           1.0 [1]                 1.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
-    ihvcn_gbar         0.0 [1]    0.0 [1]              0.0 [1]            0.5 [1]                 0.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
-    leak_gbar          1.0 [1]    0.25 [1]             0.25e-3 [1]        1.0 [1]                 1.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
+    ihvcn_gbar         0.0 [1]    0.5 [1]              0.0 [1]            0.5 [1]                 0.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
+    leak_gbar          1.0 [1]    1.0 [1]              0.25e-3 [1]        1.0 [1]                 1.0 [1]          1.0 [1]     1.0 [1]               1.0 [1]            1.0 [1]           1.0 [1] 
     leak_erev          -65. [1]   -65. [1]             -65. [1]           -65. [1]                -65. [1]         -65. [1]    -65. [1]              -65. [1]           -65. [1]          -65. [1]
     na_type            nacncoop   nacncoop             nacncoop           nacncoop                nacncoop         nacncoop    nacncoop              nacncoop           nacncoop          nacncoop
     nacncoop_vshift    0.  [2]    0. [2]               0. [2]             0. [2]                  0. [2]           0. [2]      0.  [2]               0.  [2]            0.  [2]           0.  [2] 
