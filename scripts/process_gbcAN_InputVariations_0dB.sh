3bca4398a0434aadaf4593ff8d35f96cb3418835
Paul manis <pmanis@med.unc.edu>
2020-08-11 12:57:50 -0400

    add error handling (finally)

--- scripts/process_gbcAN_InputVariations_0dB.sh
+++ scripts/process_gbcAN_InputVariations_0dB.sh
@@ -2,15 +2,29 @@
 CONFIG="autoscale_multisite_0dB_parallel.toml"
 RUNTEXT="running the individual initialization and running AN PSTH protocols"
 CELLNAMES="02 05 06 09 10 11 13 17 30"
-REPS=50
+TABLES="data_XM13A_nacncoop_normal data_XM13A_nacncoop_pasdend data_XM13A_nacncoop_actdend"
+EXPERIMENT="all=mean max=mean removelargest largestonly twolargest"
+REPS=10
+TEST="" # "--testsetup"  # or "--testsetup"
+echo $RUNTEXT
 for f in $CELLNAMES
 do
-    # python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --configfile $CONFIG
-    # python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou all=mean --configfile $CONFIG
-    # python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou max=mean --configfile $CONFIG
-    python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou removelargest --configfile $CONFIG
-    # python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou largestonly --configfile $CONFIG
-    # python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou twolargest --configfile $CONFIG
+	for t in $TABLES
+    do
+		for e in $EXPERIMENT
+		do
+		    python vcnmodel/model_run2.py VCN_c$f -H -P runANPSTH -r $REPS --Spirou $e --configfile $CONFIG --datatable $t --workers 8 $TEST
+            exit_status=$?
+            if [ "${exit_status}" -ne 0 ];
+            then
+                echo "model_run2 fails with: ${exit_status}"
+                echo "Loop was: cell=${f} table=${t}  experiment=${e}"
+                exit
+            else
+                echo "model_run2 success"
+            fi
+		done
+	done
 done
 
 wait
