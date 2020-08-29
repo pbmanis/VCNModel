7e1d177b55ad0d543092244a5fb25de077be2c92
Paul manis <pmanis@med.unc.edu>
2020-08-29 17:28:41 -0400

    script changes

--- scripts/gradeA_ANs.sh
+++ scripts/gradeA_ANs.sh
@@ -8,28 +8,29 @@
 # Note we do not have a full reconstruction for cell 18
 # in that dataset.
 #######################################################
-CELLNAMES="05 30"
+CELLNAMES="30" # "02 05 06 09 10 13 17"
 #CONFIG="noscale.toml" #"autoscale.toml"
 CONFIG="autoscale_xm13a_multisite_parallel.toml"
 RUNTEXT="running the individual initialization and running AN PSTH protocols"
+WORKERS="16"
 REPS="100"
 echo $RUNTEXT
 for f in $CELLNAMES
 do
     echo $f
     python vcnmodel/model_run2.py VCN_c$f  -F -P initAN --configfile $CONFIG  --datatable data_XM13A_nacncoop
-    python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 10 --Spirou all --configfile $CONFIG --datatable data_XM13A_nacncoop
+    python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 20 --Spirou all --workers $WORKERS --configfile $CONFIG --datatable data_XM13A_nacncoop
     if [ $? -ne 0 ]; then
         exit 1
     fi
-    python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 10 --Spirou largest --configfile $CONFIG --datatable data_XM13A_nacncoop
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-    python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 10 --Spirou removelargest --configfile $CONFIG --datatable data_XM13A_nacncoop
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
+    # python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 20 --Spirou largestonly --workers $WORKERS --configfile $CONFIG --datatable data_XM13A_nacncoop
+    # if [ $? -ne 0 ]; then
+    #     exit 1
+    # fi
+    # python vcnmodel/model_run2.py VCN_c$f  -F -P runANPSTH -r $REPS --dB 20 --Spirou removelargest --workers $WORKERS --configfile $CONFIG --datatable data_XM13A_nacncoop
+    # if [ $? -ne 0 ]; then
+    #     exit 1
+    # fi
 done
 
 # wait
