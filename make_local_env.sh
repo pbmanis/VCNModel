0180370dbc25e06cafe9f6dd87caca1ff8a4953b
Paul manis <pmanis@med.unc.edu>
2020-10-16 10:05:52 -0400

    updates to make a local vcnmodel_venv

--- make_local_env.sh
+++ make_local_env.sh
@@ -1,22 +1,24 @@
 ENVNAME="vcn_venv"
-python3 -m venv $ENVNAME
+python3.7 -m venv $ENVNAME
+
 source $ENVNAME/bin/activate
-pip install --upgrade pip  # be sure pip is up to date in the new env.
-pip install wheel  # seems to be missing (note singular)
-pip install cython
+
+pip3 install --upgrade pip  # be sure pip is up to date in the new env.
+pip3 install wheel  # seems to be missing (note singular)
+pip3 install cython
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
 #
 # #Then:
 #
-pip install -r requirements.txt
+pip3 install -r requirements_local.txt
 source $ENVNAME/bin/activate
 
 # build the mechanisms
 # this may equire a separate install of the standard NEURON package
 # with the same version as we have provided
-nrnivmodl cnmodel/mechanisms
+nrnivmodl ../../cnmodel/mechanisms
 
 # these has to be done separately as the compilation depends on what happens above
 pip3 install -e git+https://github.com/pbmanis/cochlea-1.git@c2e8c9612481ebe397ba0e7762b8f2772500388d#egg=cochlea
