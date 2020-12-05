f96fa78e54af365b8c7b5a9aa6badb209628b786
pbmanis <pmanis@med.unc.edu>
2020-12-05 09:12:31 -0500

    clean up data dirs

--- vcnmodel/retrieve_lytle_data.py
+++ vcnmodel/retrieve_lytle_data.py
@@ -21,6 +21,8 @@ if run in the non-interactive mode, 4 and 5 are skipped, and you must manually d
 once the email arrives...
 Note you must be on campus or using a VPN to access the machine this way
 """
+from dataclasses import dataclass
+from typing import Union
 import paramiko
 import sys
 
@@ -31,48 +33,53 @@ if len(sys.argv) > 1:
 else:
     machine = "Lytle"
 
-gradeACells = [2, 5, 6, 9, 10, 11, 13, 17, 30]
 
 
-class Retiever(object):
-    def __init__(self):
-        self.setPaths()
-        self.sysChoice = {  #'152.19.187.84'},
-            "Lytle": {
-                "uname": "pbmanis",
-                "dir": self.datapaths,
-                "addr": "152.19.86.111",
-            },
-            "Tule": {
-                "uname": "pbmanis",
-                "dir": self.datapaths,
-                "addr": "152.19.86.116",
-            },
-        }
+@dataclass
+class SysInfo:
+    uname: str='pbmanis'
+    directory: Union[str, Path, None]=None
+    addr: str=''
 
-        print("Machine: ", machine)
-        print(self.datapaths["baseDirectory"])
-        if machine not in self.sysChoice.keys():
-            print("Machine %s not recognized" % machine)
+
+class Syncer(object):
+    def __init__(self, remotemachine, push_pull="pull"):
+        self.push_pull = push_pull
+        self.machine = remotemachine
+        self.gradeACells = [2, 5, 6, 9, 10, 11, 13, 17, 30]
+        self.set_paths()
+        if self.machine == 'Lytle':
+            self.remote =SysInfo(directory=self.datapaths, addr="192.168.1.195")
+        elif self.machine == 'Tamalpais2':
+            self.remote = SysInfo(directory=self.datapaths, addr="192.168.1.218")
+        else:
+            print("Machine {self.machine:s} is not recognized")
             exit()
+            
+        print("Machine: ", self.machine)
+        print(self.datapaths["baseDirectory"])
 
-        mypwd = getpass.getpass("Password for %s: " % machine)
+        # set password to empty string if have passwordless ssh setup already
+        mypwd = "" # getpass.getpass("Password for %s: " % self.machine)
 
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print("connecting to: ", self.sysChoice[machine]["addr"])
-        print("as user: ", self.sysChoice[machine]["uname"])
+        print("connecting to: ", self.remote.addr)
+        print("as user: ", self.remote.uname)
         conn = self.ssh.connect(
-            self.sysChoice[machine]["addr"],
-            username=self.sysChoice[machine]["uname"],
-            password=mypwd,
+            self.remote.addr,
+            username=self.remote.uname,
+            password= mypwd,
         )  # hate putting pwd in file like this...
         if conn is False:
             print("Connection failed")
             exit()
         self.conn = conn
+    
+    def done(self):
+        self.ssh.close()
 
-    def setPaths(self, stimtype="AN", cell=11):
+    def set_paths(self, stimtype="AN", cell=11):
         where_is_data = Path("wheres_my_data.toml")
         if where_is_data.is_file():
             self.datapaths = toml.load("wheres_my_data.toml")
@@ -81,62 +88,78 @@ class Retiever(object):
         self.basepath = self.datapaths["baseDirectory"]
 
     # define system directory information...
+    def get_all_cell_data(self):
+        for ic in self.gradeACells:
+            self.get_cell_data(self, ic)
+    
+    def transfer_cell_data(self, ic:int, push_pull="push", dryRun:bool=True, daysback:int=3):
+    # gradeACells = [30]
+        self.set_paths(cell=ic)
+        remote_dir = str(Path(
+                self.remote.directory["baseDirectory"],
+                f"VCN_c{ic:02d}",
+                "Simulations",
+                "AN",
+            ))
+        local_dir = str(Path(remote_dir).parent) + '/'
+        cdcmd = "cd " + remote_dir
+        print('cdcmd: ', cdcmd)
+        # print('backup dir: ', backupDir)
 
-    def getCellData(self):
-        # gradeACells = [30]
-        for ic in gradeACells:
-            self.setPaths(cell=ic)
-            dpath = str(Path(
-                    self.sysChoice[machine]["dir"]["baseDirectory"],
-                    f"VCN_c{ic:02d}",
-                    "Simulations",
-                    "AN",
-                ))
-            cdcmd = "cd " + dpath
-            backupDir = dpath # important: end path slash!!!! 
-            print('cdcmd: ', cdcmd)
-            # print('backup dir: ', backupDir)
-
-            # Note that the exe command is a "single session", so all commands that will be done need to be concatenated
-            # using semicolons as if you were doing it from a command line on a terminal.
-            # cd will not "hold"
-            # stdin, stdout, stderr = self.ssh.exec_command(f"{cdcmd:s}; ls -la")
-            # sourcefiles = []
-            # for l in stdout.readlines():
-            #     lsp = l.split(' ')
-            #     print(lsp[-1])
-            #     sourcefiles.append(lsp[-1])
-            #
-            bwLimit = ''
-            excludeStr = ''
-            trash = ''
+        # Note that the exe command is a "single session", so all commands that will be done need to be concatenated
+        # using semicolons as if you were doing it from a command line on a terminal.
+        # cd will not "hold"
+        # stdin, stdout, stderr = self.ssh.exec_command(f"{cdcmd:s}; ls -la")
+        # sourcefiles = []
+        # for l in stdout.readlines():
+        #     lsp = l.split(' ')
+        #     print(lsp[-1])
+        #     sourcefiles.append(lsp[-1])
+        #
+        bwLimit = ''
+        excludeStr = ''
+        trash = ''
+        if dryRun:
             dryrun = '--dry-run'
-            daysback = 3
-            datelimit = f"--files-from=<(find {dpath:s} -mtime -{daysback:d}"
-            print('dpath:     ', dpath)
-            datelimit +=  "-type f -exec basename {} \;)"
-            backupDir = str(Path(backupDir).parent) + '/'
-            print('backupDir: ', backupDir)
-            #continue
-            # note this uses the brew installed rsync, V3.2.3 as of 8/28/2020. The "system" version is 2.6.9
-            cmd = f"/usr/local/bin/rsync -rtDuv {dryrun:s} --chmod=Du=rwx,Dg=rwx,Do=,Fu=rw,Fg=rw,Fo= {bwLimit:s}"
-            cmd += f" --itemize-changes --out-format='%n' -e ssh pbmanis@152.19.86.111:{dpath:s} {backupDir:s}"
-            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            logstr = ''
-            lo = ''
-            while True:
-                l = p.stdout.read(1)
-                if l == b'':
-                    break
-                lo += l.decode("utf-8")
-                # log.write(l)
-                # log.flush()
-                # logstr += l
-            ret = p.wait()
-            print(lo)
-            print(ret)
-            
-            # print('open ftp:')
+        else:
+            dryrun = ''
+        datelimit = f"--files-from=<(find {remote_dir:s} -mtime -{daysback:d}"
+        print('Remote_dir:     ', remote_dir)
+        datelimit +=  "-type f -exec basename {} \;)"
+
+        print('Local_dir: ', local_dir)
+        # return
+        # note this uses the brew installed rsync, V3.2.3 as of 8/28/2020. The "system" version is 2.6.9
+        cmd = f"/usr/local/bin/rsync -rtDuv {dryrun:s} --chmod=Du=rwx,Dg=rwx,Do=,Fu=rw,Fg=rw,Fo= {bwLimit:s}"
+        if self.push_pull == "pull":
+            cmd += f" --itemize-changes --out-format='%n' -e ssh pbmanis@{self.remote.addr:s}:{remote_dir:s} {local_dir:s}"
+        elif self.push_pull == "push":
+            cmd += f" --itemize-changes --out-format='%n' -e ssh {local_dir:s} pbmanis@{self.remote.addr:s}:{remote_dir:s}"
+        else:
+            raise ValueError('push-pull must be either push or pull')
+        
+        p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        logstr = ''
+        lo = ''
+        lo_line = ''
+        while True:
+            l = p.stdout.read(1)
+            if l == b'':
+                break
+            lo_line += l.decode("utf-8")
+            lo += lo_line
+            if lo_line[-1] == '\n':
+                print(lo_line,)
+                lo_line = ''
+            # log.write(l)
+            # log.flush()
+            # logstr += l
+        ret = p.wait()
+        # print(lo)
+        # print(ret)
+
+
+        # print('open ftp:')
 #             ftp = self.ssh.open_sftp()
 #             ftp.chdir(dpath)  # however this works for the sftp
 #             print(ftp.getcwd())
@@ -151,9 +174,10 @@ class Retiever(object):
 
 
 
-        self.ssh.close()
+
 
 
 if __name__ == "__main__":
-    R = Retiever()
-    R.getCellData()
+    R = Syncer(remotemachine = machine, push_pull='push')
+    R.transfer_cell_data(5, dryRun=True)
+    R.done()
