638211d14ee1512647b892eaa7349b73255800e8
Paul Manis <pmanis@med.unc.edu>
2021-03-08 16:38:26 -0500

    add hooks for tests

--- vcnmodel/util/__init__.py
+++ vcnmodel/util/__init__.py
@@ -3,7 +3,8 @@
 from . import combine_pdfs
 from . import hoc_swc_sectionmap
 from . import mpl_colormaps
-from . import render
-from . import show_swc
+# from . import render
+# from . import show_swc
 # from . import swc_to_hoc  # this belongs in neuronvis, and so there it is...
-from . import vcn_cell_hoc_rename
+# from . import vcn_cell_hoc_rename
+from .user_tester import UserTester
