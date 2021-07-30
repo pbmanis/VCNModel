cee6916fa604e01495cd204ec41f10daa88c443c
Paul Manis <pbmanis@Pauls-M1-mini.local>
2021-07-30 11:16:39 -0400

    clean up install scripts

--- make_local_env_M1.sh
+++ make_local_env_M1.sh
@@ -1,7 +1,20 @@
-mate ENVNAME="vcn_venv"
-python3.8 -m venv $ENVNAME
+set -e # force failure if anyting fails in script - ensuring completion
+set -o errexit
+ENVNAME="vcn_venv"
+if [ -d $ENVNAME ]
+then
+    echo "Removing previous environment: $ENVNAME"
+    set +e
+    rsync -aR --remove-source-files $ENVNAME ~/.Trash/ || exit 1
+    set -e
+    rm -R $ENVNAME
+else
+    echo "No previous environment - ok to proceed"
+fi
 
-source $ENVNAME/bin/activate
+python3.8 -m venv $ENVNAME || exit 1
+
+source $ENVNAME/bin/activate || exit 1
 
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
@@ -12,7 +25,7 @@ pip3 install cython
 #
 # #Then:
 #
-pip3 install -r requirements_local.txt
+pip3 install -r requirements_local_M1.txt || exit 1
 source $ENVNAME/bin/activate
 pip3 install mayavi
 # build the mechanisms
@@ -26,3 +39,5 @@ rm -rf cochleae
 pip3 install -e git+https://github.com/pbmanis/neuronvis.git@Python3#egg=neuronvis
 source $ENVNAME/bin/activate
 python setup.py develop
+
+echo "Success in installing acq4 environment!"
