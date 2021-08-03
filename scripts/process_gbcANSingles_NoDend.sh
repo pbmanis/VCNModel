b8592f9e9ae199c70d6a980d9028a32b94ec5e98
Paul Manis <pmanis@med.unc.edu>
2021-08-03 18:55:29 -0400

    updating/adding scripts

--- scripts/process_gbcANSingles_NoDend.sh
+++ scripts/process_gbcANSingles_NoDend.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
-CELLNAMES="02 05 06 09 10 11 13 17 30"
-CELLNAMES="09 11 13 30"
 #CONFIG="noscale.toml" #"autoscale.toml"
-CONFIG="singles_autoscale_multisite_parallel_test.toml"
-RUNTEXT="running the AN Single protocols NoDend"
-TABLES="data_XM13A_nacncoop_normal" # " data_XM13A_nacncoop_pasdend data_XM13A_nacncoop_actdend"
-DEND="NoDend"
+CONFIG="--configfile singles_multisite_parallel.toml"
+RUNTEXT="running the AN Single protocols NoDendrites"
 
+# sequentials:
+CELLNAMES="02 05 06 09 10 11 13 17 18 30"
+TABLES="data_XM13A_nacncoop_normal" # " data_XM13A_nacncoop_pasdend data_XM13A_nacncoop_actdend"
+DENDRITES="NoDend"
+NREPS="5"
 echo $RUNTEXT
+
 for t in $TABLES
     do
     for f in $CELLNAMES
         do
-        echo $f
-        python vcnmodel/model_run2.py VCN_c$f -P initAN -D $DEND --configfile $CONFIG  --datatable $t
-        python vcnmodel/model_run2.py VCN_c$f -P runANSingles -r 5 -D $DEND --configfile $CONFIG  --datatable $t
-     done
+        for d in $DENDRITES
+            do
+            AXON=""
+            case $f in
+                02 | 05)
+                    AXON="-A standardized"
+                    ;;
+            esac
+            echo $f
+            # python src/vcnmodel/model_run2.py VCN_c$f -P initAN -D $d $AXON $CONFIG  --datatable $t
+            python src/vcnmodel/model_run2.py VCN_c$f -P runANSingles -r $NREPS -D $d $AXON $CONFIG  --datatable $t
+         done
+    done
 done
 
 wait
@@ -27,5 +38,4 @@ echo AN Singles complete
 #     ls -lat VCN_Cells/$f/Simulations/AN
 #     echo " "
 # done
-
 
