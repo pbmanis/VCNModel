3547e0e7a99e943de102f06d0bb040b968c3cebe
Paul Manis <pmanis@med.unc.edu>
2022-09-19 14:20:23 -0400

    Tag 0.991 version as release for ms.

---
+++ VS_datasets_15dB_BC09_NoUninnervated.py
@@ -0,0 +1,35 @@
+"""
+These are the runs for the SAM tone data
+Run with the 0.7686 ASA density
+Cell BC09 with NoUninnervated2 (No uninnervated dendrites)
+15 dB SPL
+Manual copy from DataTableVCN using selection of runs and "print" function 
+
+Built: 7 June 2022
+
+"""
+
+
+samdata = {
+ '9U': [  # uninnervated dendrites removed
+        "runANPSTH-all-2022-06-06.09-35-17",
+        "runANPSTH-all-2022-06-06.10-15-16",
+        "runANPSTH-all-2022-06-06.10-48-04",
+        "runANPSTH-all-2022-06-06.11-17-28",
+        "runANPSTH-all-2022-06-06.11-48-06",
+        "runANPSTH-all-2022-06-06.12-16-45",
+        "runANPSTH-all-2022-06-06.23-12-02",
+        "runANPSTH-all-2022-06-06.23-35-20",
+        "runANPSTH-all-2022-06-06.23-58-34",
+    ],
+    '9I': [ # intact
+        "runANPSTH-all-2021-12-02.17-07-02",
+        "runANPSTH-all-2021-12-03.05-22-42",
+        "runANPSTH-all-2021-12-05.19-17-19",
+        "runANPSTH-all-2021-12-06.07-36-31",
+        "runANPSTH-all-2021-12-06.20-15-08",
+        "runANPSTH-all-2021-12-07.13-10-28",
+        "runANPSTH-all-2021-12-08.01-46-51",
+        "runANPSTH-all-2021-12-08.14-14-37",
+    ]
+}
