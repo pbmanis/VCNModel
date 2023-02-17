686eb5b666b6892b4884f193473518287194a48d
Paul Manis <pmanis@med.unc.edu>
2023-02-17 14:00:34 -0500

    rename file

--- vcnmodel/analyzers/__init__.py
+++ vcnmodel/analyzers/__init__.py
@@ -1,6 +1,6 @@
 # for analyzers
 # from . import analyze_run
-# from . import analyze_george_EPSCVariations
+# from . import analyze_PSCVariations
 # from . import sac
 # from . import sac_cython
 # from . import analysis
