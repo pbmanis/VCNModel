a387cd7740248f21371d25a31591bc17f5c36111
Paul Manis <pmanis@med.unc.edu>
2025-12-11 16:04:44 -0500

    let get_configuration define dict result

--- vcnmodel/util/get_data_paths.py
+++ vcnmodel/util/get_data_paths.py
@@ -5,6 +5,7 @@ All
 
 from pathlib import Path
 import toml
+import ephys.tools.get_configuration as get_configuration
 
 
 def get_data_paths():
@@ -22,13 +23,22 @@ def get_data_paths():
     Path object
         Full path to the output file
     """    
-    with open("wheres_my_data.toml", "r") as fh:
-        config = toml.load(fh)
-    config["basepath"] = config["baseDataDirectory"]
-    config["baseDataDirectory"] = Path(config["disk"], config["baseDataDirectory"])
-    config["cellDataDirectory"] = Path(config["disk"], config["cellDataDirectory"])
-    config["revcorrDataDirectory"] = Path(config["disk"], config["revcorrDataDirectory"])
-    config["baseMorphologyDirectory"] = Path(config["disk"], config["baseMorphologyDirectory"])
+    print("*** get data paths")
+    
+    if Path("wheres_my_data.toml").is_file():
+        with open("wheres_my_data.toml", "r") as fh:
+            config = toml.load(fh)
+        print("    Read config from toml file")
+    else:   # try to read a configuration file from the parent
+        dset, models = get_configuration.get_configuration("./config/models.cfg",
+                                                    check_completeness=False)
+        config = models[dset[0]]
+        print("    Read config from models.cfg file")
+    # config["basepath"] = config["baseDataDirectory"]
+    # config["baseDataDirectory"] = Path(config["disk"], config["baseDataDirectory"])
+    # config["cellDataDirectory"] = Path(config["disk"], config["cellDataDirectory"])
+    # config["revcorrDataDirectory"] = Path(config["disk"], config["revcorrDataDirectory"])
+    # config["baseMorphologyDirectory"] = Path(config["disk"], config["baseMorphologyDirectory"])
     
     return config
 
