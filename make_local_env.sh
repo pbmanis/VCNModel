4db6d01a77cfd5db0943538f695eb5d973ebcd79
Paul Manis <pmanis@med.unc.edu>
2026-01-08 11:51:23 -0500

    update version in old requirements

--- make_local_env.sh
+++ make_local_env.sh
@@ -11,12 +11,12 @@ then
 else
     echo "No previous environment - ok to proceed"
 fi
-python3.10 -m venv $ENVNAME || exit 1
+python3.12 -m venv $ENVNAME || exit 1
 source $ENVNAME/bin/activate || exit 1
 
 pip3 install --upgrade pip  # be sure pip is up to date in the new env.
 pip3 install wheel  # seems to be missing (note singular)
-pip3 install cython
+pip3 install cython==0.29.37
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
